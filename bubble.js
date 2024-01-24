4c607d46203749e4ffd6cd016994005c82e3a3ac
maggie <margaret13.may@gmail.com>
2024-01-23 22:23:38 -0600

    add files

---
+++ bubble.js
@@ -0,0 +1,33 @@
+function bubbleSort(arr) {
+  let count = 0;
+  for (let i = 0; i < arr.length; i++) {
+    let swapped = false;
+    for (let j = 0; j < arr.length - i; j++) {
+      count++;
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+        swapped = true;
+      }
+    }
+    if (!swapped) break;
+  }
+  return arr;
+}
+
+module.exports = bubbleSort;
+
+//   const swap = (arr, idx1, idx2) => {
+//     [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+//   };
+
+//   for (let i = arr.length; i > 0; i--) {
+//     for (let j = 0; j < i - 1; j++) {
+//       if (arr[j] > arr[j + 1]) {
+//         swap(arr, j, j + 1);
+//       }
+//     }
+//   }
+
+//   return arr;
